903e493eff3438058347348cb1f33b46f73142d0
MarcoGhise <MarcoGhise@users.noreply.github.com>
2017-12-14 15:17:18 +0100

    Create AAAUserAuthenticationProvider.java

---
+++ src/main/java/it/iol/oauthaaa/security/AAAUserAuthenticationProvider.java
@@ -0,0 +1,38 @@
+package it.iol.oauthaaa.security;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+
+public class AAAUserAuthenticationProvider implements AuthenticationProvider {
+
+	@Autowired
+	AAAProxy aaaProxy;
+
+	public boolean supports(Class<?> arg0) {
+		return true;
+	}
+
+	public Authentication authenticate(Authentication authentication)
+			throws AuthenticationException {
+		//boolean result = aaaProxy.isValidUser(authentication.getPrincipal().toString(), authentication.getCredentials().toString());
+		boolean result = true;
+		if (result) {
+			List<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
+			AAAUserAuthenticationToken auth = new AAAUserAuthenticationToken(
+					authentication.getPrincipal(),
+					authentication.getCredentials(), grantedAuthorities);
+
+			return auth;
+		} else {
+			throw new BadCredentialsException("Bad User Credentials.");
+		}
+	}
+	
+}
